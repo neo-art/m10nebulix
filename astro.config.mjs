9da41fd62c6a8a2832eafa76796fdf56e937f10c
Milen <neo-art@users.noreply.github.com>
2024-04-15 18:45:56 +0200

    Config files

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,7 +23,7 @@ const m2dxOptions = {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nebulix.unfolding.io",
+  site: "https://incandescent-buttercream-41ff55.netlify.app/",
   integrations: [
     icon(),
     mdx({}),
